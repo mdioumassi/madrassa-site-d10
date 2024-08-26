7d087ed76ce3947f36d5094b0a8dda499786df44
mdioumassi <mdioumassi@yahoo.fr>
2024-08-26 21:51:52 +0200

    steps register

---
+++ web/modules/custom/madrassa_registration/src/Controller/RegistrationController.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\madrassa_registration\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\Annotation\EntityType;
+use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+/**
+ * Returns responses for Madrassa Inscription routes.
+ */
+class RegistrationController extends ControllerBase {
+
+  protected SessionInterface $session;
+  protected EntityTypeManagerInterface $em;
+
+public static function create(ContainerInterface $container)
+{
+  return new static(
+    $container->get('session'),
+    $container->get('entity_type.manager')
+  );
+}
+
+public function __construct(SessionInterface $session, EntityTypeManagerInterface $entityTypeManager)
+{
+  $this->session = $session;
+  $this->em = $entityTypeManager;
+}
+  public function childrenCourse(int $enfantId)
+  {
+    $enfant = $this->em->getStorage('node')->load($enfantId);
+    $parent_id = $enfant->get('field_parent_id')->getValue()[0]['target_id'];
+    $registration_data = [
+      'child_id' => $enfantId,
+      'parent_id' => $parent_id,
+    ];
+  
+    $this->session->set('registration_data', $registration_data);
+    //$this->session->set('enfant_id', $enfantId);
+    
+    $response = new RedirectResponse('/nos-cours/type/child');
+    $response->send();
+  }
+
+  public function childrenCourseLevel(Request $request)
+  {
+    if ($request->isMethod('POST')) {
+      $data = $request->request->all();
+      dd($data);
+    }
+    $registration_data = $this->session->get('registration_data');
+    $registration_data['course_id'] = $courseId;
+    $this->session->set('registration_data', $registration_data);
+    $response = new RedirectResponse('/nos-cours/niveau/child');
+    $response->send();
+  }
+
+}
